8c794ce52a7460302b4124f00e9a79fa1eb3a3d9
naren579 <147800440+naren579@users.noreply.github.com>
2023-12-19 11:39:32 +0530

    Update Streamlit_pickle_Titanic_load_streamlit.py

--- Titanic/Streamlit_pickle_Titanic_load_streamlit.py
+++ Titanic/Streamlit_pickle_Titanic_load_streamlit.py
@@ -27,7 +27,7 @@ if uploaded_file is not None:
     try:
         df = pd.read_csv(uploaded_file)
         X,y=df.drop('Survived',axis=1),df['Survived']
-        with open('/Titanic/model_titanic.pkl', 'rb') as f:
+        with open('https://github.com/naren579/MLOPS/blob/main/Titanic/model_titanic.pkl', 'rb') as f:
             clf = pickle.load(f)
         if button_pressed:
             pred_proba=clf.predict_proba(X)[:,1]
