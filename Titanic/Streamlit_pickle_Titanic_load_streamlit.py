c9ee624f3cbfb9fece83d9590afd58bc576b5a9c
naren579 <147800440+naren579@users.noreply.github.com>
2023-12-23 19:05:02 +0530

    Update Streamlit_pickle_Titanic_load_streamlit.py

--- Titanic/Streamlit_pickle_Titanic_load_streamlit.py
+++ Titanic/Streamlit_pickle_Titanic_load_streamlit.py
@@ -14,6 +14,18 @@ import pickle
 import streamlit as st
 import pandas as pd
 
+def load_model():
+    url = 'https://raw.githubusercontent.com/naren579/MLOPS/main/Titanic/model_titanic.pkl'
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        loaded_model = pickle.loads(response.content)
+        return loaded_model
+    else:
+        st.error(f"Failed to load model. Status code: {response.status_code}")
+        return None
+
 st.set_page_config(layout="wide")
 # Streamlit app
 
@@ -28,8 +40,9 @@ if uploaded_file is not None:
     try:
         df = pd.read_csv(uploaded_file)
         X,y=df.drop('Survived',axis=1),df['Survived']
-        with open('model_titanic.pkl', 'rb') as f:
-            clf = pickle.load(f)
+        # with open('model_titanic.pkl', 'rb') as f:
+        #     clf = pickle.load(f)
+        clf=load_model()
         if button_pressed:
             pred_proba=clf.predict_proba(X)[:,1]
             y_pred=pd.DataFrame(clf.predict(X))
